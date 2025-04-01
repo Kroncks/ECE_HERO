be4b3e90bfd2a23561a2abece3c5f2d53cccf60c
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-04-01 22:51:31 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
 #include <Arduino.h>
 #include <MsTimer2.h>
-#define IN 5
-
-unsigned long etatHaut;
-unsigned long etatBas;
-float periode;
-float frequence;
-float rapportCyclique;
 
 int tempo=900; // (delay)
 
@@ -16,38 +9,22 @@ void bpm() {
   digitalWrite(8, LOW); 
 }
 
+unsigned long prev_time = 0;
+
+void rising();
+
 void setup() {
   Serial.begin(9600);
-  pinMode(IN, INPUT); // calculateur de frequence 
   pinMode(8, OUTPUT); // metronome 
   MsTimer2::set(tempo, bpm); 
   MsTimer2::start();
+  attachInterrupt(0, rising, RISING);
   delay(600);
 }
 
-void loop() {
-  etatHaut = pulseIn(IN, HIGH);
-  etatBas = pulseIn(IN, LOW);
+void loop() {}
 
-  periode = etatBas + etatHaut; //calcul de la periode totale en microsecondes 
-  frequence = 1000000/periode; // calcul de la frequence 
-  rapportCyclique = (etatHaut/periode)*100;// rapprot cyclique (pourcentage)
-
-  Serial.print("Periode : ");
-  Serial.println(periode);
-
-
-  Serial.print("Frequence : ");
-  Serial.println(frequence);
-
-
-  Serial.print("Rapport cyclique : ");
-  Serial.println(rapportCyclique);
-
-  Serial.println();
-  delay(100);
+void rising() {
+  Serial.println(micros()-prev_time);
+  prev_time = micros();
 }
-
-
-
-
